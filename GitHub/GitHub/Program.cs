ec4c9b333c45c6ad3a3b80dd7f1d6966e4f445e5
PasayevM <mansurmurad305@gmail.com>
2022-10-05 02:06:52 +0400

    Innit

---
+++ GitHub/GitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello B_C 102-in usaglarina ");
+        }
+    }
+}
